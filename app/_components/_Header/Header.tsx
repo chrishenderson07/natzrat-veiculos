4172db7d12b0d36b74bf06db494f6ebef4eb00d0
Christopher Henderson <chris.hend012@gmail.com>
2024-02-29 08:15:01 -0300

    CHORE: Config packages

--- app/_components/_Header/Header.tsx
+++ app/_components/_Header/Header.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import { Nav } from './Nav'
-
 import logo from '@/public/images/logo.svg'
+import Image from 'next/image'
+
+import { Nav } from './Nav'
 const Header = () => {
 	return (
 		<header>
