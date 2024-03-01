b7b377738f317570063b376e86f0c3b2b0d3fc0a
Christopher Henderson <chris.hend012@gmail.com>
2024-03-01 08:04:52 -0300

    CHORE: ShadCn adjusts

---
+++ app/_components/_Button/index.tsx
@@ -0,0 +1,19 @@
+import { Button as ShadButton } from '@/app/_lib/components/ui/button'
+import Link from 'next/link'
+
+interface ButtonProps {
+	path: string
+	label: string
+}
+
+const Button = ({ path, label }: ButtonProps) => {
+	return (
+		<Link
+			href={path}
+			className="rounded-2xl">
+			<ShadButton variant={'default'}>{label}</ShadButton>
+		</Link>
+	)
+}
+
+export default Button
--- app/_components/_Header/index.tsx
+++ app/_components/_Header/index.tsx
@@ -2,6 +2,7 @@ import { Nav } from './Nav'
 import logo from '@/public/images/logo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
+import Button from '../_Button'
 const Header = () => {
 	return (
 		<header>
@@ -12,7 +13,10 @@ const Header = () => {
 			<Nav />
 
 			<div className="Actions">
-				<Link href="/contato">Contato</Link>
+				<Button
+					path="/login"
+					label="Login"
+				/>
 			</div>
 		</header>
 	)
