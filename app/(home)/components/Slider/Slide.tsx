92de5870f1f3e7f4b9550708e4f169bbe3066755
Christopher Henderson <chris.hend012@gmail.com>
2024-03-08 08:18:30 -0300

    feat: sliders works

--- app/(home)/components/Slider/Slide.tsx
+++ app/(home)/components/Slider/Slide.tsx
@@ -1,14 +1,37 @@
+import { useEffect, useRef } from 'react'
+
 import Button from '@/app/_components/_Button'
 import Image from 'next/image'
+
+import gsap from 'gsap'
 
 interface SlideProps {
 	heading: string
 	paragraph: string
 	buttonPath: string
 	imageSrc: string
+	index: number
 }
 
-const Slide = ({ heading, paragraph, buttonPath, imageSrc }: SlideProps) => {
+const Slide = ({
+	heading,
+	paragraph,
+	buttonPath,
+	imageSrc,
+	index,
+}: SlideProps) => {
+	const imageRef = useRef(null)
+
+	useEffect(() => {
+		const tl = gsap.timeline({ defaults: { duration: 1 } })
+
+		tl.to(imageRef.current, {
+			y: 0,
+			opacity: 1,
+			ease: 'power4.inOut',
+			duration: 1.5,
+		})
+	}, [])
 	return (
 		<div className="container h-screen max-h-[700px] grid grid-cols-3">
 			<div className="col-span-1 flex flex-col justify-center gap-12">
@@ -23,8 +46,10 @@ const Slide = ({ heading, paragraph, buttonPath, imageSrc }: SlideProps) => {
 			</div>
 			<div className="col-span-2 flex items-center">
 				<Image
+					ref={imageRef}
 					src={imageSrc}
 					alt="Jeep Compass"
+					className="translate-y-[50px] opacity-0 imagetest"
 				/>
 			</div>
 		</div>
--- app/(home)/components/Slider/index.tsx
+++ app/(home)/components/Slider/index.tsx
@@ -2,23 +2,65 @@
 
 import Slide from './Slide'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { useEffect, useState } from 'react'
+
 const Slider = () => {
-	const carsSlide = {
-		heading: 'Jeep Compass',
-		paragraph:
-			'O Jeep Compass é um SUV médio produzido pela Jeep, uma divisão da Stellantis. Foi lançado em 2006 para o mercado de 2007, e é vendido globalmente, exceto nos Estados Unidos, onde o Jeep Patriot e o Jeep Cherokee (KJ) foram substituídos pelo Jeep Compass e pelo Jeep Renegade.',
-		buttonPath: '/jeep-compass',
-		imageSrc: require('@/public/images/suv_banner.png'),
-	}
+	const [activeIndex, setActiveIndex] = useState(0)
+	const carsSlide = [
+		{
+			heading: 'Jeep Compass',
+			paragraph:
+				'O Jeep Compass é um SUV médio produzido pela Jeep, uma divisão da Stellantis. Foi lançado em 2006 para o mercado de 2007, e é vendido globalmente, exceto nos Estados Unidos, onde o Jeep Patriot e o Jeep Cherokee (KJ) foram substituídos pelo Jeep Compass e pelo Jeep Renegade.',
+			buttonPath: '/jeep-compass',
+			imageSrc: require('@/public/images/suv_banner.png'),
+		},
+		{
+			heading: 'Audi Sport',
+			paragraph:
+				'A divisão Audi Sport representa o ápice da performance dentro da marca alemã. Seus modelos combinam design sofisticado com tecnologia de ponta, proporcionando uma experiência de condução inigualável.',
+			buttonPath: '/jeep-compass',
+			imageSrc: require('@/public/images/sedan_banner.png'),
+		},
+		{
+			heading: 'Ferrari F8 Spider',
+			paragraph:
+				'O Ferrari F8 Spider representa a mais recente evolução da tradição de conversíveis de alto desempenho da Ferrari. Inspirado no legado da marca e impulsionado por tecnologia de ponta, este modelo oferece uma experiência de condução visceral e inigualável.',
+			buttonPath: '/jeep-compass',
+			imageSrc: require('@/public/images/sport_banner.png'),
+		},
+	]
 
 	return (
 		<>
-			<Slide
-				heading={carsSlide.heading}
-				paragraph={carsSlide.paragraph}
-				buttonPath={carsSlide.buttonPath}
-				imageSrc={carsSlide.imageSrc}
-			/>
+			<Swiper
+				spaceBetween={50}
+				slidesPerView={1}
+				navigation
+				loop={true}
+				modules={[Pagination, Navigation]}
+				autoplay={{ delay: 2500, disableOnInteraction: false }}
+				onSlideChange={(swiper) => {
+					setActiveIndex(swiper.activeIndex)
+					console.log(`Slide ${swiper.activeIndex} em tela!`)
+				}}>
+				{carsSlide.map((slide, index) => (
+					<SwiperSlide key={index}>
+						<Slide
+							index={activeIndex}
+							heading={slide.heading}
+							paragraph={slide.paragraph}
+							buttonPath={slide.buttonPath}
+							imageSrc={slide.imageSrc}
+						/>
+					</SwiperSlide>
+				))}
+			</Swiper>
 		</>
 	)
 }
